af377ac7779619bb21cd81bee08968e243540ad7
ciela <zektbach@gmail.com>
2019-05-13 16:51:18 +0900

    Add encoder and decoder

---
+++ decoder.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+
+class Decoder(nn.Module):
+
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+    def forward(self, *input):
+        return super().forward(*input)
---
+++ encoder.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+    def forward(self, *input):
+        return super().forward(*input)
