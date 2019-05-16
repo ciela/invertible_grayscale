509a91da8c3c1afd49fa58a2f5a7bccf2d825f02
ciela <zektbach@gmail.com>
2019-05-16 16:24:44 +0900

    Move application code of util module

--- models.py
+++ models.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import util
-
 
 class ResidualBlock(nn.Module):
 
--- train.py
+++ train.py
@@ -1,14 +1,15 @@
 import torch
 
 from models import Encoder, Decoder
+import util
 
 
 # TODO: delete
 def main():
     encoder = Encoder()
     print(encoder)
-    # pil_img = util.pil_loader('image_path')
-    # img_tensor = util.DEFAULT_TRANSFORM(pil_img).unsqueeze(0)
+    pil_img = util.pil_loader('image_path')
+    img_tensor = util.DEFAULT_TRANSFORM(pil_img).unsqueeze(0)
     img_tensor = torch.randn((3, 256, 256)).view(1, 3, 256, 256)
     grayscale = encoder(img_tensor)
     print(grayscale)
