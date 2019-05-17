e599a576cae70fae01688383064ca7d3ce03558c
ciela <zektbach@gmail.com>
2019-05-17 18:42:08 +0900

    Add loss module

---
+++ loss.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn.
+
+
+def invertibility(orig: torch.Tensor, restored: torch.Tensor):
+    pass
+
+
+def grayscale_conformity(orig: torch.Tensor, grayscale: torch.Tensor):
+    pass
+
+
+def quantization(grayscale: torch.Tensor):
+    pass
