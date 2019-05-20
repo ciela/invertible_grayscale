be05f9a6004ce945da041fd0bd4df79698d0497d
ciela <zektbach@gmail.com>
2019-05-20 18:44:38 +0900

    Implement Loss class a little

--- loss.py
+++ loss.py
@@ -1,14 +1,45 @@
 import torch
-import torch.nn.
+import torch.nn
+import torch.nn.functional as F
 
 
-def invertibility(orig: torch.Tensor, restored: torch.Tensor):
-    pass
+class Loss(nn.Module):
 
+    def __init__(self, full_weights: tuple, gc_weights: tuple):
+        super(Loss, self).__init__()
+        self.fw = full_weights
+        self.gcw = gc_weights
 
-def grayscale_conformity(orig: torch.Tensor, grayscale: torch.Tensor):
-    pass
+    def forward(self, orig: torch.Tensor, grayscale: torch.Tensor, restored: torch.Tensor) -> torch.Tensor:
+        interbility = self.fw[0] * Loss.invertibility(orig, restored)
+        grayscale_conformity = self.fw[1] * Loss.grayscale_conformity(orig, grayscale, self.gcw)
+        quantization = self.fw[2] * Loss.quantization(grayscale)
+        full_loss = interbility + grayscale_conformity + quantization
+        return full_loss
 
+    @staticmethod
+    def invertibility(orig: torch.Tensor, restored: torch.Tensor) -> torch.Tensor:
+        return F.mse_loss(orig, restored)
 
-def quantization(grayscale: torch.Tensor):
-    pass
+    @staticmethod
+    def grayscale_conformity(orig: torch.Tensor, grayscale: torch.Tensor, weights: tuple) -> torch.Tensor:
+        lightness = weights[0] * gc_lightness(orig, grayscale)
+        contrast = weights[1] * gc_contrast(orig, grayscale)
+        local_structure = weights[2] * gc_local_structure(orig, grayscale)
+        return lightness + contrast + local_structure
+
+    @staticmethod
+    def gc_lightness(orig: torch.Tensor, grayscale: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @staticmethod
+    def gc_contrast(orig: torch.Tensor, grayscale: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @staticmethod
+    def gc_local_structure(orig: torch.Tensor, grayscale: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @staticmethod
+    def quantization(grayscale: torch.Tensor) -> torch.Tensor:
+        pass
