9b25902622e2c29cf3253f921dda7039cdcbfa78
ciela <zektbach@gmail.com>
2019-05-21 12:04:41 +0900

    Calculate loss

--- train.py
+++ train.py
@@ -3,20 +3,26 @@ import torch
 
 from models import Encoder, Decoder
 import util
+from loss import Loss
 
 
 @click.command()
 @click.option('-i', '--imgpath', type=str)
 def main(imgpath):
-    encoder = Encoder()
-    print(encoder)
-    img_tensor = util.img_to_tensor(img_path=imgpath).unsqueeze(0)
+    gpu = torch.device('cuda:0')
+    criterion = Loss(weights=(1, 1, 1), gc_weights=(1, 1, 1))
+    encoder = Encoder().to(gpu)
+    print(list(encoder.parameters()))
+    img_tensor = util.img_to_tensor(img_path=imgpath).unsqueeze(0).to(gpu)
     grayscale = encoder(img_tensor)
     print(grayscale.size())
-    decoder = Decoder()
-    print(decoder)
+    decoder = Decoder().to(gpu)
+    print(list(decoder.parameters()))
     resotred = decoder(grayscale)
     print(resotred.size())
+    loss = criterion(img_tensor, grayscale, resotred)
+    print(loss)
+    loss.backward()
 
 
 if __name__ == "__main__":
