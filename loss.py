2a989a7427420f4c1eba75acfccaaebab29d4b18
ciela <zektbach@gmail.com>
2019-05-21 19:48:38 +0900

    Implement lightness and quantization losses

--- loss.py
+++ loss.py
@@ -1,45 +1,59 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.transforms as transforms
 
 
 class Loss(nn.Module):
 
-    def __init__(self, weights: tuple, gc_weights: tuple):
+    def __init__(self,
+        weights1: tuple = (1.0, 0.0), weights2: tuple = (0.5, 10.0),
+        gc_weights: tuple = (1e-7, 0.5), gc_lightness_theta: int = 70):
         super(Loss, self).__init__()
-        self.w = weights
+        self.w1 = weights1
+        self.w2 = weights2
         self.gcw = gc_weights
+        self.theta = gc_lightness_theta
 
-    def forward(self, orig: torch.Tensor, grayscale: torch.Tensor, restored: torch.Tensor) -> torch.Tensor:
-        interbility = self.w[0] * Loss.invertibility(orig, restored)
-        grayscale_conformity = self.w[1] * Loss.grayscale_conformity(orig, grayscale, self.gcw)
-        quantization = self.w[2] * Loss.quantization(grayscale)
-        full_loss = interbility + grayscale_conformity + quantization
+    def forward(self, orig_color: torch.Tensor, orig_gray: torch.Tensor,
+        grayscale: torch.Tensor, restored: torch.Tensor, stage: int = 1) -> torch.Tensor:
+        weights = self.w1 if stage == 1 else self.w2
+        invertibility = self.invertibility(orig_color, restored)
+        grayscale_conformity = weights[0] * self.grayscale_conformity(orig_gray, grayscale)
+        quantization = weights[1] * self.quantization(grayscale)
+        full_loss = invertibility + grayscale_conformity + quantization
         return full_loss
 
-    @staticmethod
-    def invertibility(orig: torch.Tensor, restored: torch.Tensor) -> torch.Tensor:
-        return F.mse_loss(orig, restored)
+    def invertibility(self, orig_color: torch.Tensor, restored: torch.Tensor) -> torch.Tensor:
+        return F.mse_loss(orig_color, restored)
 
-    @staticmethod
-    def grayscale_conformity(orig: torch.Tensor, grayscale: torch.Tensor, weights: tuple) -> torch.Tensor:
-        lightness = weights[0] * Loss.gc_lightness(orig, grayscale)
-        contrast = weights[1] * Loss.gc_contrast(orig, grayscale)
-        local_structure = weights[2] * Loss.gc_local_structure(orig, grayscale)
+    def grayscale_conformity(self, orig_gray: torch.Tensor, grayscale: torch.Tensor) -> torch.Tensor:
+        lightness = self.gc_lightness(orig_gray, grayscale)
+        contrast = self.gcw[0] * self.gc_contrast(orig_gray, grayscale)
+        local_structure = self.gcw[1] * self.gc_local_structure(orig_gray, grayscale)
         return lightness + contrast + local_structure
 
-    @staticmethod
-    def gc_lightness(orig: torch.Tensor, grayscale: torch.Tensor) -> torch.Tensor:
+    def gc_lightness(self, orig_gray: torch.Tensor, grayscale: torch.Tensor) -> torch.Tensor:
+        # abs range is [0, 2]
+        return torch.mean(
+            torch.max(
+                torch.abs(grayscale - orig_gray) - self.theta / 127,
+                torch.zeros_like(orig_gray)))
+
+    def gc_contrast(self, orig_gray: torch.Tensor, grayscale: torch.Tensor) -> torch.Tensor:
+        # TODO: Use the conv4_4 layer from VGG
+        return F.mse_loss(orig_gray, orig_gray)
+
+    def gc_local_structure(self, orig: torch.Tensor, grayscale: torch.Tensor) -> torch.Tensor:
+        # TODO: Calculate diffs of Total-Variations
         return F.mse_loss(orig, orig)
 
-    @staticmethod
-    def gc_contrast(orig: torch.Tensor, grayscale: torch.Tensor) -> torch.Tensor:
-        return F.mse_loss(orig, orig)
-
-    @staticmethod
-    def gc_local_structure(orig: torch.Tensor, grayscale: torch.Tensor) -> torch.Tensor:
-        return F.mse_loss(orig, orig)
-
-    @staticmethod
-    def quantization(grayscale: torch.Tensor) -> torch.Tensor:
-        return F.mse_loss(grayscale, grayscale)
+    def quantization(self, grayscale: torch.Tensor) -> torch.Tensor:
+        grayscale_stack = torch.stack([grayscale for _ in range(256)])
+        M = torch.zeros_like(grayscale)
+        M_stack = torch.stack([M.new_full(M.size(), fill_value=d) for d in range(256)])
+        M_stack = (M_stack / 127.5) - 1
+        return torch.mean(
+            torch.min(
+                torch.min(
+                    torch.abs(grayscale_stack - M_stack), dim=3).values, dim=2).values)
