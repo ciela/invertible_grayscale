58d8194bfa560e6ca82dd1b6bc7e29d219bb05d0
ciela <zektbach@gmail.com>
2019-05-25 16:56:08 +0900

    Set learning rate of optimizer

--- train.py
+++ train.py
@@ -13,18 +13,17 @@ def main(imgpath):
     gpu = torch.device('cuda:0')
     invertible_grayscale = InvertibleGrayscale()
     print(invertible_grayscale)
-    optim = Adam(invertible_grayscale.parameters())
+    optim = Adam(invertible_grayscale.parameters(), lr=0.0002)
     criterion = Loss()
     pil_img = util.pil_loader(img_path=imgpath)
     X_color, T_gray = util.img_to_tensor(pil_img=pil_img)
     X_color, T_gray = X_color.unsqueeze(0), T_gray.unsqueeze(0)
-
     for i in range(10):
         optim.zero_grad()
         Y_grayscale, Y_restored = invertible_grayscale(X_color)
         loss = criterion(X_color, T_gray, Y_grayscale, Y_restored, stage=1)
         print(loss)
-        loss.backward(retain_graph=True)
+        loss.backward()
         optim.step()
 
 
